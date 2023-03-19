d423c49db2c0f19c012bb36179b61eab9e890237
Maierl01 <lukasmaier-bewerbung@outlook.de>
2023-03-19 18:47:08 +0100

    kjkj

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,124 +2,125 @@ import './App.css';
 import ImageComponent from './components/ImageComponent';
 import TopBar from './components/TopBar';
 import FooterComponent from './components/FooterComponent';
+import { useEffect, useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import React from "react";
+import io from 'socket.io-client';
 
-export default class App extends React.Component {
+const socket = io.connect('http://localhost:1234');
 
-  constructor(props) {
-    super(props);
-    this.state = {
-        isLoading: true,
-        error: null,
-        popup: false,
-        popuperror: false,
-        objects: [],
-        bids:[],
-        bidhighest: [],
-    };
+export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [popup, setPopup] = useState(false);
+  const [popupError, setPopupError] = useState(false);
+  const [objects, setObjects] = useState([]);
+  const [bids, setBids] = useState([]);
+  const [bidhighest, setBidHighest] = useState([]);
 
-      this.ObjectGet = this.ObjectGet.bind(this);
-    }
-
-    async componentDidMount() {
-      await this.ObjectGet();
-    }
-    async handleError() {
-      this.setState({popuperror: true});
-    }
-    async handlepopup() {
-      this.setState({popup: true});
-    }
-    async handleClose() {
-      this.setState({popup: false, popuperror: false});
-    }
-
-   
-    
-      async ObjectGet() {
-        try {
-            const result = await fetch('http://localhost:1234/auction');
-            if (result.status === 200) {
-                const objects = await result.json();
-                console.log(objects);
-                console.log("Objekte wurden geladen")
-                this.setState({ objects });
-            } else {
-                throw Error("Keine Objekte gefunden!");
-            }
-        } catch (error) {
-            console.log(error);
-            this.setState({ error });
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const objectResult = await fetch('http://localhost:1234/auction');
+        if (objectResult.status === 200) {
+          const objects = await objectResult.json();
+          console.log(objects);
+          console.log("Objekte wurden geladen")
+          setObjects(objects);
+        } else {
+          throw Error("Keine Objekte gefunden!");
         }
-        this.setState({ isLoading: false });
-        try {
-          const result = await fetch('http://localhost:1234/bids');
-            if (result.status === 200) {
-                const bidnormal = await result.json();
-                const bids = bidnormal.reverse();
-                console.log(bids);
-                console.log("Gebote wurden geladen")
-                this.setState({ bids });
-            } else {
-                throw Error("Keine Gebote gefunden!");
-            }
-        } catch (error) {
-            console.log(error);
-            this.setState({ error });
-        }
-        this.setState({ isLoading: false });
-        try {
-          const result = await fetch('http://localhost:1234/bids/highest');
-            if (result.status === 200) {
-                const bidhighest = await result.json();
-                console.log(bidhighest);
-                console.log("Gebote wurden geladen")
-                this.setState({ bidhighest });
-            } else {
-                throw Error("Keine Gebote gefunden!");
-            }
-        } catch (error) {
-            console.log(error);
-            this.setState({ error });
-        }
-        this.setState({ isLoading: false });
+      } catch (error) {
+        console.log(error);
+        setError(error);
       }
 
-
-      async BidObject() {
-        try {
-          const result = await fetch("http://localhost:1234/bid", {
-          method: "POST",
-          mode: "cors",
-          headers: {
-            "Content-type": "application/json",
-          },
-          body: JSON.stringify({ 
-              "nutzer" : 11
-            }),
-        });
-       console.log(result);
-            if (result.status === 200) {
-                console.log("erfolgreich")
-            } else {
-                throw Error("Interner Server Fehler.");
-            }
-        } catch (error) {
-            console.log(error);
-            this.setState({error});
+      try {
+        const bidResult = await fetch('http://localhost:1234/bids');
+        if (bidResult.status === 200) {
+          const bidnormal = await bidResult.json();
+          const bids = bidnormal.reverse();
+          console.log(bids);
+          console.log("Gebote wurden geladen")
+          setBids(bids);
+        } else {
+          throw Error("Keine Gebote gefunden!");
         }
-        this.setState({isLoading: false});
-        }
+      } catch (error) {
+        console.log(error);
+        setError(error);
+      }
 
-    render() {
-      const { err, isLoading, objects, bids, bidhighest} = this.state;
-      if (err) {
-          return <div>Sorry, etwas ist schiefgelaufen!</div>;
+      try {
+        const highestBidResult = await fetch('http://localhost:1234/bids/highest');
+        if (highestBidResult.status === 200) {
+          const bidhighest = await highestBidResult.json();
+          console.log(bidhighest);
+          console.log("Gebote wurden geladen")
+          setBidHighest(bidhighest);
+        } else {
+          throw Error("Keine Gebote gefunden!");
+        }
+      } catch (error) {
+        console.log(error);
+        setError(error);
+      }
+
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  function updateBids(data) {
+    setBids(prevBids => [...prevBids, data]);
+  }
+
+  function handleBidChange() {
+    const bid = document.querySelector('input').value;
+    socket.emit('bid-did-change', bid);
+  }
+
+  function handlePopup() {
+    setPopup(true);
+  }
+
+  function handleError() {
+    setPopupError(true);
+  }
+
+  function handleClosePopup() {
+    setPopup(false);
+    setPopupError(false);
+  }
+
+  async function handleBidObject() {
+    try {
+      const result = await fetch("http://localhost:1234/bid", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ "nutzer": 11 }),
+      });
+      console.log(result);
+      if (result.status === 200) {
+        console.log("erfolgreich")
+      } else {
+        throw Error("Interner Server Fehler.");
+      }
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    }
+    setIsLoading(false);
+  }
+
+  if (error) {
+          return <div>Seite kann nicht geladen werden</div>;
       } else if (isLoading) {
-          return <div>Loading....</div>;
+          return <div>Seite lädt....</div>;
       } else {
       return (
         <div className="App">
@@ -143,29 +144,30 @@ export default class App extends React.Component {
           <div className='pt-3'></div>
           <Row className="pt-5 ps-5"> 
               <Col className="pt-4 pb-3 block-verlauf" xl={12} >
-                  <p className="headline">AKTUELLES GEBOT: {bidhighest.score} €</p>
-                <Button  onClick={() => this.BidObject()} className="button1">Jetzt {bidhighest.score + 500} € bieten</Button><br/>
-                <div className="description">
-                  Restzeit: {objects.auktionszeit} <br/>
-                  Starthöhe: {objects.starthöhe} € <br/>
-                </div>
-                <div className="rechtliches pt-2">
-                  Rechtliche Hinweise: "Ihre Gebote bei dieser Auktion sind verbindlich und Sie sind rechtlich verpflichtet,
-                  den Artikel zum gebotenen Preis zu erwerben, sollten Sie die Auktion gewinnen." 
+                <div className="bieten">
+                    <p className="headline">AKTUELLES GEBOT: {bidhighest.score} €</p>
+                  <Button  onClick={() => this.BidObject()} id="Button" name="Button" className="button1">Jetzt {bidhighest.score + 500} € bieten</Button><br/>
+                  <div className="description">
+                    Restzeit: {objects.auktionszeit} <br/>
+                    Starthöhe: {objects.starthöhe} € <br/>
+                  </div>
+                  <div className="rechtliches pt-2">
+                    Rechtliche Hinweise: "Ihre Gebote bei dieser Auktion sind verbindlich und Sie sind rechtlich verpflichtet,
+                    den Artikel zum gebotenen Preis zu erwerben, sollten Sie die Auktion gewinnen." 
+                  </div>
                 </div>
               </Col>
 
               <Col className="pt-4 pb-5 block-verlauf" lg={6} sm={12}>
                 <p className="headline">Gebotsverlauf:</p>
-                {bids.map((bid)=> (
-                  <div className='description'>
-                    {bid.value} bietet {bid.score} €
-                    <div className="border border-1 border-secondary"></div>
-                  </div>
-                ))}
-                  
-                  
-                
+                <div className="gebotsverlauf">
+                  {bids.map((bid)=> (
+                    <div className='description'>
+                      {bid.value} bietet {bid.score} €
+                      <div className="border border-1 border-secondary"></div>
+                    </div>
+                  ))}
+                </div>
               </Col>
             </Row>
           <FooterComponent/>
@@ -173,4 +175,3 @@ export default class App extends React.Component {
       );
     }
   }
-}
